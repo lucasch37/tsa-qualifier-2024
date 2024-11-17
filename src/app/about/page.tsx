e48adf9fcb5743addc14119325cc0766e5621edf
Pranith Nagarajan <pranith.nag@outlook.com>
2024-11-17 15:39:58 -0800

    Filled about and faq page with content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,33 +1,36 @@
 "use client";
 
-import React, {useState} from "react";
-import {motion} from "framer-motion";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const boxes = [
     {
         id: 1,
         title: "Company",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        className: "top-0 left-0"
+        content:
+            "Our company, the RHS Student Store, was founded in 2021 and is a student-led initiative which aims to fund school events, programs, and more. Appearing on Shark Tank in 2023 for our innovative and unique lobster slippers, we were able to get one particular shark to bite at our signature product, the lobster slippers. Securing a $300,000 deal with Mark Cuban, we were able to expand on the slippers and continue our journey to make people's lives more exciting through fashion.",
+        className: "top-0 left-0",
     },
     {
         id: 2,
         title: "Creators",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        className: "top-0 right-0"
+        content: "We are a group of 5 students from Redmond High School who created this product page. We are passionate about bringing joy and fun to our school and community. We hope you enjoy our website and our product, the lobster slippers!",
+        className: "top-0 right-0",
     },
     {
         id: 3,
         title: "Inspiration",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        className: "bottom-0 left-0"
+        content:
+            "We looked around Redmond High School and saw that students footwear was lacking in creativity. We wanted to change that and spice things up, bringing a sense of fun and whimsy to school. What better way to do that than with lobster slippers? These slippers turn heads in the hallways and make people smile. We wanted to bring that joy to everyone, and that's why we created the lobster slippers",
+        className: "bottom-0 left-0",
     },
     {
         id: 4,
         title: "Mission",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        className: "bottom-0 right-0"
-    }
+        content:
+            "Our mission is to bring comfort, uniqueness, and pizzazz to consumers with our high-quality product, the lobster slippers. Blending the unique and whimsical design with comfortable practicality, we aim to make every single part of the process create the best user experience possible.",
+        className: "bottom-0 right-0",
+    },
 ];
 
 export default function About() {
@@ -42,21 +45,27 @@ export default function About() {
                         key={box.id}
                         className={`absolute ${box.className} bg-accent [background-image:radial-gradient(88%_100%_at_top,rgba(255,255,255,0.5),rgba(255,255,255,0))] rounded-lg shadow-md overflow-hidden`}
                         style={{
-                            zIndex: hoveredId === box.id ? 10 : 1
+                            zIndex: hoveredId === box.id ? 10 : 1,
                         }}
                         initial={{
                             width: "calc(50% - 20px)",
                             height: "calc(50% - 20px)",
-                            margin: 20
+                            margin: 20,
                         }}
                         animate={{
-                            width: expandedId === box.id ? "calc(100% - 240px)" : "calc(50% - 20px)",
-                            height: expandedId === box.id ? "calc(100% - 240px)" : "calc(50% - 20px)",
+                            width:
+                                expandedId === box.id
+                                    ? "calc(100% - 240px)"
+                                    : "calc(50% - 20px)",
+                            height:
+                                expandedId === box.id
+                                    ? "calc(100% - 240px)"
+                                    : "calc(50% - 20px)",
                         }}
                         transition={{
                             type: "spring",
                             stiffness: 200,
-                            damping: 25
+                            damping: 25,
                         }}
                         onHoverStart={() => {
                             setExpandedId(box.id);
@@ -73,24 +82,30 @@ export default function About() {
                                 initial={{
                                     left: "50%",
                                     top: "50%",
-                                    transform: "translate(-50%, -50%)"
+                                    transform: "translate(-50%, -50%)",
                                 }}
                                 animate={{
                                     left: expandedId === box.id ? "0" : "50%",
-                                    top: expandedId === box.id ? "0" : "calc(50% - 2rem)",
-                                    transform: expandedId === box.id ? "none" : "translate(-50%, -50%)"
+                                    top:
+                                        expandedId === box.id
+                                            ? "0"
+                                            : "calc(50% - 2rem)",
+                                    transform:
+                                        expandedId === box.id
+                                            ? "none"
+                                            : "translate(-50%, -50%)",
                                 }}
                                 transition={{
-                                    duration: 0.2
+                                    duration: 0.2,
                                 }}
                             >
                                 {box.title}
                             </motion.h2>
                             {expandedId === box.id && (
                                 <motion.div
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{delay: 0.2}}
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    transition={{ delay: 0.2 }}
                                     className="text-zinc-800 mt-12"
                                 >
                                     {box.content}
@@ -102,4 +117,4 @@ export default function About() {
             </div>
         </div>
     );
-};
+}
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -5,32 +5,83 @@ import {
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-} from "@/components/ui/accordion"
-
+} from "@/components/ui/accordion";
 
 export default function Home() {
     return (
         <div className="flex flex-col gap-6 justify-center items-center mx-auto max-w-3xl pb-20 w-full h-full">
-            <h1 className="text-6xl font-bold font-bebas">Frequently Asked Questions</h1>
+            <h1 className="text-6xl font-bold font-bebas">
+                Frequently Asked Questions
+            </h1>
             <Accordion type="single" collapsible className="w-full space-y-4">
-            <AccordionItem value="item-1" className="bg-complement rounded-lg px-4 shadow-md border-none">
-                <AccordionTrigger className="text-4xl font-bebas">Customer Satisfaction Guarantee</AccordionTrigger>
-                <AccordionContent className="text-lg">
-                Made you look.
-                </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2" className="bg-complement rounded-lg px-4 shadow-md border-none">
-                <AccordionTrigger className="text-4xl font-bebas">Return Policy</AccordionTrigger>
-                <AccordionContent className="text-lg">
-                Made you look.
-                </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3" className="bg-complement rounded-lg px-4 shadow-md border-none">
-                <AccordionTrigger className="text-4xl font-bebas">Caring for Your Slippers</AccordionTrigger>
-                <AccordionContent className="text-lg">
-                Made you look.
-                </AccordionContent>
-            </AccordionItem>
+                <AccordionItem
+                    value="item-1"
+                    className="bg-complement rounded-lg px-4 shadow-md border-none"
+                >
+                    <AccordionTrigger className="text-4xl font-bebas">
+                        Customer Satisfaction Guarantee
+                    </AccordionTrigger>
+                    <AccordionContent className="text-lg">
+                        We guarantee your satisfaction <strong>100%</strong> of the time on every
+                        product we sell, and will refund your purchase price if
+                        you are not fully satisfied. We can also replace any
+                        slippers if they come with defects or any manufacturing
+                        issues.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem
+                    value="item-2"
+                    className="bg-complement rounded-lg px-4 shadow-md border-none"
+                >
+                    <AccordionTrigger className="text-4xl font-bebas">
+                        Return Policy
+                    </AccordionTrigger>
+                    <AccordionContent className="text-lg">
+                        You may return your product for a full refund within 60
+                        days of purchase provided the product is in close to new
+                        condition and has not been worn outside. Please request
+                        a return label from us and you can either print it at
+                        home or pick it up from a nearby post office.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem
+                    value="item-3"
+                    className="bg-complement rounded-lg px-4 shadow-md border-none"
+                >
+                    <AccordionTrigger className="text-4xl font-bebas">
+                        Caring for Your Slippers
+                    </AccordionTrigger>
+                    <AccordionContent className="text-lg">
+                        <ul className="mt-4 list-disc space-y-2">
+                            <li>
+                                <strong>Cleaning:</strong> Every now and then to
+                                clean your slippers use a damp cloth and a mild
+                                soap with circular motions and avoid soaking the
+                                slippers.
+                            </li>
+                            <li>
+                                <strong>Drying:</strong> If the slippers are
+                                affected by any water, make sure to air dry them
+                                and avoid heat sources as they can damage the
+                                integrity of the slippers.
+                            </li>
+                            <li>
+                                <strong>Integrity:</strong> When not in use,
+                                stuff the slippers with some light paper in
+                                order to maintain their shape and integrity.
+                                Also, avoid putting heavy items on top of
+                                slippers and store them in a cool and dry place.
+                            </li>
+                            <li>
+                                <strong>Odor:</strong> Use a pinch of baking
+                                soda and let the slippers sit overnight for the
+                                soda to absorb the odors. Then shake them out
+                                the next day and use a vacuum to remove any
+                                leftover powder.
+                            </li>
+                        </ul>
+                    </AccordionContent>
+                </AccordionItem>
             </Accordion>
         </div>
     );
